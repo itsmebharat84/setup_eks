dbc53df51f8cf34e899dc7bbd804bed90712f50b
itsmebharat84 <itsme.bharat84@gmail.com>
2022-09-16 14:36:25 +0530

    initial commit

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  version    = "~> 3.0"
+  region     = var.aws_region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type = string
+}
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
