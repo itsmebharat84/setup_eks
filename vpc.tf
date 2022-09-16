d2085911d5ebf133a83f556ed398d6dbd7fabf80
itsmebharat84 <itsme.bharat84@gmail.com>
2022-09-16 16:32:24 +0530

    add vpc

--- vpc.tf
+++ vpc.tf
@@ -6,14 +6,6 @@ locals {
   }
 }
 
-/*
-resource "aws_vpc" "setup-eks-vpc" {
-  cidr_block = "10.17.0.0/24"
-
-  tags = local.tags
-}
-*/
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.4"
@@ -24,4 +16,11 @@ module "vpc" {
   azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
   private_subnets = ["10.17.0.0/27", "10.17.0.32/27", "10.17.0.64/27"]
   public_subnets  = ["10.17.0.128/27", "10.17.0.160/27", "10.17.0.192/27"]
+
+  enable_dns_support               = "false"
+  enable_nat_gateway               = "false"
+  single_nat_gateway               = "false"
+  assign_generated_ipv6_cidr_block = "false"
+
+  tags = local.tags
 }
