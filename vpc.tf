66b4484d12595748c8a3fff028f4aa79bd475f63
itsmebharat84 <itsme.bharat84@gmail.com>
2022-09-16 16:28:07 +0530

    add vpc

--- vpc.tf
+++ vpc.tf
@@ -18,10 +18,10 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.4"
 
-  name       = var.name
-  cidr_block = "10.17.0.0/24"
+  name = var.name
+  cidr = "10.17.0.0/24"
 
-  availability_zones         = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
-  private_subnet_cidr_blocks = ["10.17.0.0/27", "10.17.0.32/27", "10.17.0.64/27"]
-  public_subnet_cidr_blocks  = ["10.17.0.128/27", "10.17.0.160/27", "10.17.0.192/27"]
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets = ["10.17.0.0/27", "10.17.0.32/27", "10.17.0.64/27"]
+  public_subnets  = ["10.17.0.128/27", "10.17.0.160/27", "10.17.0.192/27"]
 }
