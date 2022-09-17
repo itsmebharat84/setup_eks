37d9cec854af4860207b7a9d268dbbfbf3459fea
itsmebharat84 <itsme.bharat84@gmail.com>
2022-09-17 19:21:31 +0530

    update vpc

--- vpc.tf
+++ vpc.tf
@@ -34,7 +34,7 @@ module "vpc" {
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/cluster/${var.name}" = "shared"
     "kubernetes.io/role/internal-elb"     = 1
   }
 
